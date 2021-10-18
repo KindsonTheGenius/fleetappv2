c29d13c3510baaa3c41b9b9db1215f75d17038fc
KindsonTheGenius <Smart24*>
2021-10-18 21:33:53 +0200

    Commit on Oct-18-21

---
+++ src/main/java/com/kindsonthegenius/fleetapp_v2/security/models/Role.java
@@ -0,0 +1,4 @@
+package com.kindsonthegenius.fleetapp_v2.security.models;
+
+public class Role {
+}
---
+++ src/main/java/com/kindsonthegenius/fleetapp_v2/security/services/RoleService.java
@@ -0,0 +1,4 @@
+package com.kindsonthegenius.fleetapp_v2.security.services;
+
+public class RoleService {
+}
